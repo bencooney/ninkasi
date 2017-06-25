29bec63648f80afc2556a3e3717bb8a94e00f8e4
Ben <ben@localhost.localdomain>
2017-06-25 22:10:48 +1000

    exploring Firmata

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+router.get('/arduino', function(req, res){
+	var five = require("johnny-five");
+	var board = new five.Board();
+
+	board.on("ready", function() {
+	  var led = new five.Led(13);
+	  led.blink(500);
+	});
+
+	res.send('arduino');
+});
+
+
 module.exports = router;
